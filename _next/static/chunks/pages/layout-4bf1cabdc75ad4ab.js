6c89ac78478e8e12871a46d4228635b41a7dfb12
keinnerross <keinnerross@gmail.com>
2024-12-11 23:38:43 -0300

    first commit

---
+++ _next/static/chunks/pages/layout-4bf1cabdc75ad4ab.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[482],{5158:(n,_,e)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/layout",function(){return e(4776)}])},4776:(n,_,e)=>{"use strict";e.r(_),e.d(_,{default:()=>t});var u=e(4848);function t(n){let{children:_}=n;return(0,u.jsx)("section",{children:_})}}},n=>{var _=_=>n(n.s=_);n.O(0,[636,593,792],()=>_(5158)),_N_E=n.O()}]);
